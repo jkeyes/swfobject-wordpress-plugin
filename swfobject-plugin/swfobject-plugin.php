88961c66a9d6e548f69fec1b444838673744c555
John Keyes <johnkeyes@gmail.com>
2009-03-27 00:52:23 +0000

    Added 12seconds support, templated the Javascript.

--- swfobject-plugin/swfobject-plugin.php
+++ swfobject-plugin/swfobject-plugin.php
@@ -27,6 +27,74 @@ Author URI: http://keyes.ie
 
 define('SWFOBJECT_PLUGIN_URL_PATH', get_option('siteurl') . '/wp-content/plugins/' . dirname(plugin_basename(__FILE__)) . '/');
 
+$swfobject_script = <<<SWFOBJECT_SCRIPT
+<script type="text/javascript">
+if(typeof(swfobject) == "undefined") {
+        var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.setAttribute("src", "PLUGIN_URL_PATHswfobject.js");
+    script.setAttribute("type", "text/javascript");
+    head.appendChild(script);
+}
+</script>
+SWFOBJECT_SCRIPT;
+$swfobject_script = str_replace("PLUGIN_URL_PATH", SWFOBJECT_PLUGIN_URL_PATH, $swfobject_script);
+define('SWFOBJECT_SCRIPT', $swfobject_script);
+
+$template = <<<TEMPLATE
+<script type="text/javascript">
+var flashvars  = _FLASHVARS_;
+var params     = _PARAMS_;
+var attributes = {};
+swfobject.embedSWF("_URL_",
+                   "_SITE_-VID_ID",
+                   "WIDTH",
+                   "HEIGHT",
+                   "9.0.0",
+                   "expressInstall.swf",
+                   flashvars,
+                   params,
+                   attributes);
+</script>
+<div id="_SITE_-VID_ID"></div>
+TEMPLATE;
+define('TEMPLATE', $template);
+
+function get_javascript($site, $flashvars, $params, $url)
+{
+    $tokens = array( '_SITE_', '_FLASHVARS_', '_PARAMS_', '_URL_');
+    $values = array( $site, $flashvars, $params, $url);
+    return str_replace($tokens, $values, TEMPLATE);
+}
+
+$qik = get_javascript(
+        "qik",
+        "{ 'rssURL': 'http://qik.com/video/VID_ID.rss', 'autoPlay': 'false' }",
+        "{ 'allowScriptAccess': 'sameDomain', 'allowFullScreen': 'true', 'movie': 'http://qik.com/swfs/qikPlayer4.swf', 'quality': 'high', 'bgcolor': '#333333' }",
+        'http://qik.com/swfs/qikPlayer4.swf'
+);    
+
+$tweleve_seconds = get_javascript(
+        "12seconds",
+        "{'vid': 'VID_ID' }",
+        "{'movie': 'http://embed.12seconds.tv/players/remotePlayer.swf' }",
+        'http://embed.12seconds.tv/players/remotePlayer.swf'
+);
+
+$vimeo = get_javascript(
+        "vimeo",
+        "{}",
+        "{}",
+        'http://vimeo.com/moogaloop.swf?clip_id=VID_ID&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1'
+);
+
+$youtube = get_javascript(
+        "youtube",
+        "{}",
+        "{}",
+        'http://www.youtube.com/v/VID_ID'
+);
+
 // YouTube 
 define("VIDEO_CONFIG", 
         serialize(
@@ -35,20 +103,27 @@ define("VIDEO_CONFIG",
                                 "width" => 425,
                                 "height" => 344,
                                 "regexp" => "/\[youtube ([[:print:]]+)\]/",
-                                "code" => "<script type=\"text/javascript\">swfobject.embedSWF(\"http://www.youtube.com/v/VID_ID\", \"youtube-VID_ID\", \"WIDTH\", \"HEIGHT\", \"9.0.0\",\"expressInstall.swf\", {}, {}, {});</script><div id=\"youtube-VID_ID\"></div>"
+                                "code"   => $youtube
                         ),
                         "vimeo" => array(
                                 "width" => 400,
                                 "height" => 300,
                                 "regexp" => "/\[vimeo ([[:print:]]+)\]/",
-                                "code" => "<script type=\"text/javascript\">var flashvars = {};var params = {};var attributes = {};swfobject.embedSWF(\"http://vimeo.com/moogaloop.swf?clip_id=VID_ID&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1\", \"vimeo-VID_ID\", \"WIDTH\", \"HEIGHT\", \"9.0.0\",\"expressInstall.swf\", flashvars, params, attributes);</script><div id=\"vimeo-VID_ID\"></div>"
+                                "code" => $vimeo
                         ),
                         "qik" => array (
                         		"width" => 425,
                         		"height" => 319,
                                 "regexp" => "/\[qik ([[:print:]]+)\]/",
-                                "code" => "<script type=\"text/javascript\">var flashvars = {'rssURL': 'http://qik.com/video/VID_ID.rss', 'autoPlay': 'false' };var params = { 'allowScriptAccess': 'sameDomain', 'allowFullScreen': 'true', 'movie': 'http://qik.com/swfs/qikPlayer4.swf', 'quality': 'high', 'bgcolor': '#333333' };var attributes = {};swfobject.embedSWF(\"http://qik.com/swfs/qikPlayer4.swf\", \"qik-VID_ID\", \"WIDTH\", \"HEIGHT\", \"9.0.0\",\"expressInstall.swf\", flashvars, params, {});</script><div id=\"qik-VID_ID\"></div>"
+                                "code" => $qik
+                        ),
+                        "12seconds" => array (
+                        		"width" => 425,
+                        		"height" => 319,
+                                "regexp" => "/\[12seconds ([[:print:]]+)\]/",
+								"code" => $tweleve_seconds
                         )
+                        
                 )
         )
 );
@@ -76,16 +151,13 @@ function video_plugin($content)
         $config = unserialize(VIDEO_CONFIG);
         $first = 0;
         foreach ($config as $key => $value) {
-                if ($first == 0) { add_swfobject_script(); $first = 1; }
+                if ($first == 0) {
+                	echo(SWFOBJECT_SCRIPT); 
+                	$first = 1; 
+                }
                 $content = (preg_replace_callback($config[$key]["regexp"], 'video_plugin_callback', $content));
         }
         return $content; 
-}
-
-function add_swfobject_script() {
-        echo('<script type="text/javascript">if(typeof(swfobject) == "undefined") {     var head = document.getElementsByTagName("head")[0];   
-        var script = document.createElement("script");  script.setAttribute("src", "'.SWFOBJECT_PLUGIN_URL_PATH.'swfobject.js");        script.
-setAttribute("type", "text/javascript");        head.appendChild(script); }</script>');
 }
 
 add_filter('the_content', 'video_plugin',1);
